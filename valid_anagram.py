a927f5c482e489e4a96861afa23eb1a28924f08f
Cristina Rosace <tina.rosace@gmail.com>
2022-08-06 14:36:32 -0400

    added an additional anagram solution

--- valid_anagram.py
+++ valid_anagram.py
@@ -7,3 +7,26 @@ def is_anagram(s, t):
 
     return False
 
+def is_anagram_2(s,t):
+    if len(s) != len(t):
+        return False
+
+    s_letters = {}
+    t_letters = {}
+
+    for i in range(len(s)):
+        if s[i] not in s_letters:
+            s_letters[s[i]] = 1
+        else:
+            s_count = s_letters.get(s[i]) + 1
+            s_letters[s[i]] = s_count
+
+        if t[i] not in t_letters:
+            t_letters[t[i]] = 1
+        else:
+            t_count = t_letters.get(t[i]) + 1
+            t_letters[t[i]] = t_count
+
+    return s_letters == t_letters
+
+
